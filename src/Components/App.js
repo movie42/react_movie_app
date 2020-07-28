a0c59f2be676712bd44d949dc5e9ca8824807267
movie42 <44064122+movie42@users.noreply.github.com>
2020-07-28 19:10:12 +0900

    BrowserRouter with Switch and Redirct

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 import Router from "Components/Router";
+import Header from "Components/Header";
 
-function App() {
-  return (
-    <>
-      <Router />
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Router />
+      </>
+    );
+  }
 }
 
 export default App;
